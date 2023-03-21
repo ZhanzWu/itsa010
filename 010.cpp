e549110f10f2d80d6f5b593e6cedf1c4a4e96ca1
使用者 <@LAPTOP-EOFH6302>
2023-03-21 20:59:55 +0800

    加入專案檔案。

---
+++ 010.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, t;
+        cin >> a >> b;
+      while( b!=0 )
+        {
+            t = b;
+            b = a%b;
+            a = t;
+
+        }
+     cout << a << endl;
+             return 0;
+}
